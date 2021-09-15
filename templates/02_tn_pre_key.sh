04e15badd2729ca21d22f82e37581930d6be7c49
Anders Åslund <anders.aslund@teknoir.ai>
2021-09-15 10:37:04 +0200

    Fixing conditions

--- templates/02_tn_pre_key.sh
+++ templates/02_tn_pre_key.sh
@@ -1,7 +1,7 @@
 verify_downloader curl || verify_downloader wget || fatal 'Can not find curl or wget for downloading files'
 
 SUDO=''
-if [ ${EUID} -ne 0  && -z "${OS_BUILD+x}" ]; then
+if [ ${EUID} -ne 0 ] && [ -z "${OS_BUILD+x}" ]; then
     info "Please be ready to enter the device´s sudo password:"
     SUDO='sudo -H'
 fi
