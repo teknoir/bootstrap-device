ef62cd4c1c203a455ea9b4820a7387d29d4063eb
Anders Åslund <anders.aslund@teknoir.ai>
2023-08-15 08:02:16 +0200

    WIP

--- templates/00_tn_header.sh
+++ templates/00_tn_header.sh
@@ -10,7 +10,8 @@ set -o noglob
 #   TBD
 #
 # Environment variables:
-#   TBD:
-#   INSECURE=true
-#   INSTALL_CALICO=true
+#   OS_BUILD=true (default: false, for OS builds)
+#   INSECURE=true (default: false, for OS builds, or if you want to run without ssl cert verification)
+#   INSTALL_WATCHDOG=false (default: true, enable or disable the teknoir watchdog service)
+#   USE_DOCKER=false (default: true, enable or disable the use of docker as container runtime for k3s)
 
--- templates/09_tn_pre_calico.sh
+++
@@ -1,3 +0,0 @@
-if [ "${INSTALL_CALICO}" = true ]; then
-    info "Install Calico Manifests"
-    $SUDO tee /var/lib/rancher/k3s/server/manifests/calico.yaml > /dev/null << EOL
