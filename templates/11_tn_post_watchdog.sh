372d58318779a83447cb871566be63afd7a9a697
Anders Åslund <anders.aslund@teknoir.ai>
2023-08-16 13:47:26 +0200

    Watchdog OS build

--- templates/11_tn_post_watchdog.sh
+++ templates/11_tn_post_watchdog.sh
@@ -1,1 +1,9 @@
+  $SUDO chmod 644 /lib/systemd/system/tn-watchdog.service
+  info "Enable and start watchdog service"
+  if [ ${OS_BUILD} ]; then
+      info "OS Build specifics"
+      $SUDO ln -s /etc/systemd/system/multi-user.target.wants/tn-watchdog.service /lib/systemd/system/tn-watchdog.service
+  else
+      $SUDO systemctl enable --now tn-watchdog.service
+  fi
 fi
