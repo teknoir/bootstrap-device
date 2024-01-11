6f63eaa45f6804c5b516a8dd07ddd9f71eac99f7
Jacob Clodfelter <jacob.clodfelter@teknoir.ai>
2024-01-11 11:55:49 -0700

    Changed /etc service location for watchdog

--- templates/10_tn_post_watchdog.sh
+++ templates/10_tn_post_watchdog.sh
@@ -1,8 +1,8 @@
-  $SUDO chmod 644 /lib/systemd/system/tn-watchdog.service
+  $SUDO chmod 644 /etc/systemd/system/tn-watchdog.service
   info "Enable and start watchdog service"
   if [ ${OS_BUILD} ]; then
       info "OS Build specifics"
-      $SUDO ln -s /lib/systemd/system/tn-watchdog.service /etc/systemd/system/multi-user.target.wants/tn-watchdog.service
+      $SUDO ln -s /etc/systemd/system/tn-watchdog.service /etc/systemd/system/multi-user.target.wants/tn-watchdog.service
   else
       $SUDO systemctl enable --now tn-watchdog.service
   fi
